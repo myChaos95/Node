e9a4a74ea4d35d6232d51d9c1fa233cc66303fd4
chaos <mychaos95@gmail.com>
2017-06-23 08:24:17 +0800

    no

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var app = express();
+
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', (req ,res) => {
+	req.send('hello world');
+})
+
+router.get('/ok', (req ,res ) => {
+	req.send('fine');
+})
 
 module.exports = router;
